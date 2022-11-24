c9e42a647db262ee92b0428dadebd668515541d2
Piotr <Piotr@xprpio>
2022-11-24 07:20:07 +0100

    Add include

--- main.c
+++ main.c
@@ -6,3 +6,13 @@
  */
 
 
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include <stdlib.h>
+#include <string.h>
+#include "timers/timers.h"
+#include "ifRS485/ifRS485.h"
+#include "ds18b20/ds18b20.h"
+#include "ds18b20/pindef.h"
+#include "ds18b20/onewire.h"
